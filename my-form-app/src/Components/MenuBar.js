3e0adcd618f640d80bb64c4bb9693c687978891c
Manojdeep Dakavaram <mada1380@colorado.edu>
2024-05-02 22:21:25 -0600

    Added Data Collector and Analyzer

--- my-form-app/src/Components/MenuBar.js
+++ my-form-app/src/Components/MenuBar.js
@@ -13,9 +13,10 @@ const MenuBar = ({ anchorEl,isOpen, handleClose, handleMenuClick }) => {
           'aria-labelledby': 'basic-button',
         }}
       >
-        <MenuItem onClick={() => handleMenuClick('All Recipies')}>All Recipies</MenuItem>
+        <MenuItem onClick={() => handleMenuClick('All Recipes')}>All Recipies</MenuItem>
         <MenuItem onClick={() => handleMenuClick('My Recipes')}>My Recipies</MenuItem>
         <MenuItem onClick={() => handleMenuClick('My Saved Posts')}>My Saved Posts</MenuItem>
+        <MenuItem onClick={() => handleMenuClick('Most Liked Recipes')}>Most Liked Recipes</MenuItem>
         <MenuItem onClick={() => handleMenuClick('Logout')}>Logout</MenuItem>
       </Menu>
   );
